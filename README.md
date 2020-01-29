23c5500c7afa87dca04622b87ea8a4d8e1ebec7d
Mikko Aro <M9492@student.jamk.fi>
2020-01-29 17:40:16 +0200

    Add buttons and location image

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# W.I.P
+# React-demo  
+
+## API  
+
+There are over 450 TMS-stations in the Finnish read network. TMS-stations contain sensors that collect computational data from traffic. [Digitraffic.fi](https://www.digitraffic.fi/) offers open data about Finnish road-, railway- and marine traffic. API gives data in JSON-format, direct link to data is [here](https://tie.digitraffic.fi/api/v3/metadata/tms-stations).  
+
+## Application  
+
+Application fetches current traffic data from TMS-station located on the south side of Muurame (coordinates: 62.095101405389734, 25.628549623379794). My parents-in-law lives in Jämsä, due this, our red Toyota Corolla often heads into the south. With the TMS data I am trying to answer the eternal question "Who's drivin?". If TMS-station indicates that traffic is jamming – perhaps it's my turn to relax. If there's barely any traffic, I can volunteer to drive and enjoy the empty roads.  
+
+Application show bar chart with following data:  
+
+* The average speed from the last five minutes.  
+
+* The average speed from the given 60 minutes time period.  
+
+* The average speed percentage of the road free flow speed from the last five minutes.
+
+  * Over 90: Traffic is fluent.
+
+  * 75-90: Traffic is platooning.
+
+  * 25-70: Traffic is slow.
+
+  * 10-25: Traffic is queuing.
+
+  * 0-10: Traffic is stationary
+
+* Percentage of vehicles passed during the fixed 60 minutes time window from the maximum amount of vehicles per hour
+
+## Link to [application]()  
--- src/App.js
+++ src/App.js
@@ -1,74 +1,137 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Moment from 'react-moment';
-import Chart from './components/Chart'
+import Chart from './components/Chart';
+import lam from './images/LAM.PNG';
 
 function App() {
   const url = "https://tie.digitraffic.fi/api/v1/data/tms-data/23922"
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
   const [options, setOptions] = useState({})
-  const [percentage, setpercentage] = useState(1);
   const [date, setDate] = useState('')
-  
+  const [data, setData] = useState([])
+
   useEffect(() => {
     setLoading(true)
-      async function fetchData(url){
-        try {
-          const response = await fetch(url);
-          const json = await response.json();
-          setOptions(parseData(json));
-          setLoading(false)
-        }
-        catch(error) {
-          setError(true)
-        }
-      } 
+    async function fetchData(url) {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+        setOptions(parseData(json));
+      } catch (error) {
+        setError(true)
+      }
+    }
     fetchData(url);
   }, []);
 
-   function parseData(data){
+  function parseData(data) {
     var date = data.tmsStations[0].measuredTime
     setDate(date)
     var parsedData = data.tmsStations[0].sensorValues
+    setData(parsedData)
     return chartDetails(parsedData);
   }
 
-  function chartDetails(data) {
-    //setpercentage(80);
-    const options = {
-      labels: [data[12].name, data[13].name, data[14].name, data[15].name],
-      datasets:[{
-        label: 'Jyväskylä - Jämsä',
-        data: [data[12].sensorValue, data[13].sensorValue, data[14].sensorValue, data[15].sensorValue, 9],
-        backgroundColor: [
-          'rgba(255, 70, 100, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)'
-        ],
-        borderWidth: 1
-      }]
+  function chartDetails(data, destination) {
+    if (destination === undefined) {
+      const options = {
+        labels: [],
+        datasets: [{
+          label: '',
+          data: [],
+          backgroundColor: [],
+          borderColor: [],
+          borderWidth: 1
+        }]
+      }
+      setLoading(false)
+      return options;
     }
-
-    return options;
+    if (destination === "Jyväskylä") {
+      const options = {
+        labels: [
+          ['Average speed (km/h)', '(5 min)'],
+          ['Average speed (km/h)', '(60 min)'],
+          ['Average speed (%)', '(5 min)', '(>90 traffic is fluent)'],
+          ['Vehicle count (%) from maximum amount', '(60 min)']
+        ],
+        datasets: [{
+          label: 'Jämsä - Jyväskylä',
+          data: [data[12].sensorValue, data[2].sensorValue, data[14].sensorValue, data[7].sensorValue],
+          backgroundColor: [
+            'rgba(252, 214, 112, 1)',
+            'rgba(252, 185, 65, 1)',
+            'rgba(137, 196, 244, 1)',
+            'rgba(75, 192, 192, 0.5)',
+          ],
+          borderColor: [
+            'rgba(235, 149, 50, 1)',
+            'rgba(235, 149, 50, 1)',
+            'rgba(44, 130, 201, 1)',
+            'rgba(75, 192, 192, 1)',
+          ],
+          borderWidth: 1
+        }]
+      }
+      setOptions(options)
+      return options;
+    }
+    if (destination === "Jämsä") {
+      const options = {
+        labels: [
+          ['Average speed (km/h)', '(5 min)'],
+          ['Average speed (km/h)', '(60 min)'],
+          ['Average speed (%)', '(5 min)', '(>90 traffic is fluent)'],
+          ['Vehicle count (%) from maximum amount', '(60 min)']
+        ],
+        datasets: [{
+          label: 'Jyväskylä - Jämsä',
+          data: [data[13].sensorValue, data[3].sensorValue, data[15].sensorValue, data[9].sensorValue],
+          backgroundColor: [
+            'rgba(252, 214, 112, 1)',
+            'rgba(252, 185, 65, 1)',
+            'rgba(137, 196, 244, 1)',
+            'rgba(75, 192, 192, 0.5)',
+          ],
+          borderColor: [
+            'rgba(235, 149, 50, 1)',
+            'rgba(235, 149, 50, 1)',
+            'rgba(44, 130, 201, 1)',
+            'rgba(75, 192, 192, 1)',
+          ],
+          borderWidth: 1
+        }]
+      }
+      setOptions(options)
+      return options;
+    }
   }
 
-  if (error) return(<div style={{color: 'red'}}>Error occured while connecting to API</div>)
-  if (loading) return(<div style={{color:'blue'}}>Loading...</div>)
+  function givenDestination(destination, props){
+    chartDetails(data, destination)
+  }
+
+  if (error) return(<div className="error-message">Error occured while connecting to API</div>)
+  if (loading) return(<div className="load-message">Loading...</div>)
   return (
-    <div className="Chart">
-      <div>
+    <div>
+      <div className="chart" style={{ width: 1000, height: 600}}>
         <Moment date={date} />
         <Chart data={options}/>
+      </div>
+      <div className="cont" style={{ width: 1000, height: 600}}>
+        <div className="button-container">
+        <p>Destination:</p>
+          <button onClick={(e) => givenDestination("Jyväskylä", e)}>Jyväskylä</button>
+          <button onClick={(e) => givenDestination("Jämsä", e)}>Jämsä</button>
+        </div>
+        <div className="image-container">
+          <img className="lam" src={lam} alt="TMS location" />
+          <p>TMS-station location</p>
+        </div>
       </div>
     </div>
   )
